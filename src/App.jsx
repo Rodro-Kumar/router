52fd78672777b313546e2f9a26e67e8b9b4bec43
Rodro Kumar <rudrokormokar@gmail.com>
2024-03-08 01:09:51 +0600

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Header from "./Component/Header"
+import About from "./Component/About"
+import Contact from "./Component/Contact"
+import Footer from "./Component/Footer"
+import Nav from "./Component/Nav"
+import Home  from "./Component/Home"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav/>
+    <Home/>
+<Header/>
+<About/>
+<Contact/>
+<Footer/>
+    </>
+  )
+}
+
+export default App
