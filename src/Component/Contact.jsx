b74673968c7fb029e37ac2a5eac33253dd66809d
Rodro Kumar <rudrokormokar@gmail.com>
2024-03-08 21:39:10 +0600

    update

---
+++ src/Component/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='container bg-red-500 text-white text-3xl text-center my-2'>Contact</div>
+  )
+}
+
+export default Contact
