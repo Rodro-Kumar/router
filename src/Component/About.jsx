b3d88afb4a2f14bb0b14e4fbf63fed65f6a945ab
Rodro Kumar <rudrokormokar@gmail.com>
2024-03-08 21:35:10 +0600

    update

---
+++ src/Component/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container bg-yellow-500 text-black text-3xl text-center mt-2'>About</div>
+  )
+}
+
+export default About
